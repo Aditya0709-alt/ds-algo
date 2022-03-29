d335de6ae0585e36ccb8071bb3446f613c0e206a
Aditya Pawar <77115883+Aditya0709-alt@users.noreply.github.com>
2022-03-29 21:00:34 +0530

    0-1 knap done

--- DP/0-1Knapsack.java
+++ DP/0-1Knapsack.java
@@ -18,3 +18,27 @@ static int knapsack(int [] wt, int [] val, int W, int n){
             return knapsack(wt,val,W,n-1);
         
     }
+
+static int max(int a, int b){
+        return (a > b) ? a : b;
+    }
+    
+	static int knapsack(int [] wt, int [] val, int W, int n){
+	    int i, j;
+	    int [][] t = new int[n+1][W+1];
+	    
+        for(i = 0; i < n+1; i++){
+            for(j = 0; j < W+1; j++){
+                if(i ==0 || j == 0){
+                    t[i][j] = 0;
+                }
+                else if(wt[i-1] <= j){
+                    t[i][j] = max(val[i-1] + t[i-1][j - wt[i-1]], t[i-1][j]);
+                }
+                else {
+                    t[i][j] = t[i-1][j];
+                }
+            }
+        }
+        return t[n][W];
+    }
