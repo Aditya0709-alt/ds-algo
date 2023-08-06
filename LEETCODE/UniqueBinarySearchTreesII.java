34e3fa566ac23f90c283d446bb03f7bfcda9b3df
Aditya Pawar <77115883+Aditya0709-alt@users.noreply.github.com>
2023-08-06 16:29:12 +0530

    Create UniqueBinarySearchTreesII.java

---
+++ LEETCODE/UniqueBinarySearchTreesII.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) {
+            return new ArrayList<>();
+        }
+        return generateTrees(1, n);
+    }
+
+    private List<TreeNode> generateTrees(int start, int end) {
+        List<TreeNode> trees = new ArrayList<>();
+        
+        if (start > end) {
+            trees.add(null);
+            return trees;
+        }
+        
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> leftSubtrees = generateTrees(start, i - 1);
+            List<TreeNode> rightSubtrees = generateTrees(i + 1, end);
+            
+            for (TreeNode left : leftSubtrees) {
+                for (TreeNode right : rightSubtrees) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = left;
+                    root.right = right;
+                    trees.add(root);
+                }
+            }
+        }
+        
+        return trees;
+    }
+}
+
+
+/*
+Approach 
+We use recursion to construct all the possible trees. We check if start is greater than end, then we cannot create a subtree. 
+If the condition holds true, we generate the left and right subtree and add it to our array
+*/
